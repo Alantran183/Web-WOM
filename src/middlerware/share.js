dbef7592f499722735dc511630eb87bef4a165b4
phucLan183 <tpllazero@gmail.com>
2021-10-01 00:38:21 +0700

    update code

--- src/middlerware/share.js
+++ src/middlerware/share.js
@@ -1,6 +1,6 @@
 const Category = require('../models/Category')
 
 module.exports = async (req, res, next) => {
-  res.locals.categories = await Category.find().limit(5)
+  res.locals.categories = await Category.find({ status: "ACTIVE" })
   next()
 }
