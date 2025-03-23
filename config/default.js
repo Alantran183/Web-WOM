67bd7f744674ec49539c1b481dea3e4569e62b1c
AlanTran183 <tpllazero@gmail.com>
2025-03-23 11:36:29 +0000

    Update db

--- config/default.js
+++ config/default.js
@@ -5,6 +5,6 @@ module.exports = {
         view_folder: __dirname + '/../src/views',
         view_engine: 'ejs',
         file_upload: __dirname + '/../src/public/image',
-        mongo_url: 'mongodb+srv://tranphuclan:tranphuclan@contact.qgcrz.mongodb.net/WorldOfModel?retryWrites=true&w=majority'
+        mongo_url: 'mongodb+srv://Alan:lYUCqllT8nNb39zD@cluster0.1dawt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
     },
 }
